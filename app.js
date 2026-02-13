3ac1025f387f9abc2b5b80c285a1d17f13c76d92
Usman-Nasir-JS <musmanproducts@gmail.com>
2026-02-13 11:12:38 +0500

    Colouring

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 console.log("Hello World!");
 
+
 var handelColor = "";
 let previousDiv = null;
 
 function selectColor(div, colors) {
+
     console.log(previousDiv);
     
     if (previousDiv != null) {
@@ -13,12 +15,15 @@ function selectColor(div, colors) {
     handelColor = colors;
     previousDiv = div;
     div.style.borderWidth = '3.5px';
-}
+
+};
 
 function changeColor(div) {
+
     console.log("handelColor ===>", handelColor);
     
     div.style.backgroundColor = handelColor;
     div.style.borderWidth = handelColor;
     div.style.borderColor = "black";
-}
+
+};
